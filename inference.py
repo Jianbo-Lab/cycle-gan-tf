150fd48bc5783744a511a8ce40fd1c6bbe26d731
Jianbo Chen <jianbochen@berkeley.edu>
2017-04-24 00:48:48 -0700

    change inference

--- inference.py
+++ inference.py
@@ -17,7 +17,7 @@ random.seed(123)
 # User defined variables
 ##########################################
 TARGET = 'celebA'
-MODEL_FILE = './models/celebA/model.ckpt-20000'
+MODEL_FILE = './log/celebA/model.ckpt-1000'
 
 
 BATCH_SIZE = 1
