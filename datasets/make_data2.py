fcbbb66bd91848a9f5a3b1b000649625787de374
Jianbo Chen <jianbochen@berkeley.edu>
2017-04-24 00:11:21 -0700

    add dataset celebA

---
+++ datasets/make_data2.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np 
+import tensorflow as tf 
+import matplotlib.pyplot as plt   
+import h5py # for reading our dataset 
+
+with h5py.File(''.join(['faces_dataset_new.h5']), 'r') as hf:
+    faces = hf['images'].value
+    headers = hf['headers'].value
+    labels = hf['label_input'].value
+
+# extract Young
+labels = labels[:,-1]
+faces = (faces/255.)
+
+import scipy.misc
+ind_young = 0
+ind_old = 0
+for i,face in enumerate(faces):
+    label = labels[i]
+    if label == 1:
+        if ind_young < 1000:
+            ind_young += 1
+            scipy.misc.imsave('celebA/trainA/image-{}.jpg'.format(i),face.reshape((64,64,3)))
+        elif ind_young < 1100:
+            ind_young += 1
+            scipy.misc.imsave('celebA/testA/image-{}.jpg'.format(i),face.reshape((64,64,3)))
+    else:
+        if ind_old < 1000:
+            ind_old += 1
+            scipy.misc.imsave('celebA/trainB/image-{}.jpg'.format(i),face.reshape((64,64,3)))
+        elif ind_old < 1100:
+            ind_old += 1
+            scipy.misc.imsave('celebA/testB/image-{}.jpg'.format(i),face.reshape((64,64,3)))
---
+++ make_data.py
@@ -0,0 +1,69 @@
+# import numpy as np 
+# from os import listdir
+# from os.path import isfile, join
+# import matplotlib.image as mpimg
+# def extract_files(path):
+# 	filenames = [f for f in listdir(path)]
+# 	for filename in filenames:
+# 		header = filename.split('.')[0]
+# 		birthyear = header.split('-')[1]
+# 		photoyear = header.split('-')[-1]
+# 		age = int(photoyear) - int(birthyear)
+
+# 		image = mpimg.imread(filename)
+# 		print image.shape
+from glob import glob
+import numpy as np
+import scipy.misc
+import matplotlib.pyplot as plt 
+import os
+import pandas as pd
+import h5py
+import tqdm
+data = glob(os.path.join("img_align_celeba", "*.jpg"))
+data = np.sort(data)
+def imread(path):
+	return scipy.misc.imread(path).astype(np.float)
+
+def resize_width(image, width=64.):
+	h, w = np.shape(image)[:2]
+	return scipy.misc.imresize(image,[int((float(h)/w)*width),width])
+		
+def center_crop(x, height=64):
+	h= np.shape(x)[0]
+	j = int(round((h - height)/2.))
+	return x[j:j+height,:,:]
+
+def get_image(image_path, width=64, height=64):
+	return center_crop(resize_width(imread(image_path), width = width),height=height)
+
+dim = 64 
+
+images = np.zeros((len(data),dim*dim*3), dtype = np.uint8)
+
+# make a dataset
+for i in tqdm.tqdm(range(len(data))):
+    #for i in tqdm.tqdm(range(10)):
+    image = get_image(data[i], dim,dim)
+    images[i] = image.flatten()
+
+attribute_file = 'list_attr_celeba.txt'
+
+with open(attribute_file, 'r') as f:
+    num_examples = f.readline()
+    headers = f.readline()
+headers = headers.split()
+
+label_input = pd.read_fwf(attribute_file,skiprows=2,
+                       widths = [10,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3, 
+                                 3,3,3,3,3,3,3,3,3,3,3],
+                   index_col=0,
+                   header=None
+                  )
+
+labels = label_input.astype(int).as_matrix()
+
+with h5py.File(''.join(['datasets/faces_dataset_new.h5']), 'w') as f:
+    dset_face = f.create_dataset("images", data = images)
+    dset_headers = f.create_dataset('headers', data = headers)
+    dset_label_input = f.create_dataset('label_input', data = label_input)
